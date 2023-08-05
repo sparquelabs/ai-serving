a5dacc8d243eb075588ea0b0e67bef29ef03ea80
Sanjay Mijar <sanjay@kontain.app>
2023-08-05 03:49:59 +0000

    k3s configuration steps

---
+++ k3s-aws-ecr-setup.sh
@@ -0,0 +1,23 @@
+# sets up k3s for using the ECR registry - likely token expires in 12 hours so this might have 
+#   to be part of a cron job
+set -x
+export ECR_TOKEN=`aws ecr get-login-password --region "us-east-1"`
+aws ecr get-login-password --region "us-east-1"  | docker login --username AWS --password-stdin ${MY_ECR_REPOSITORY}
+
+# add the ECR repo login
+sudo cat<<EOF >> /tmp/registries.yaml
+mirrors:
+    docker.io:
+        endpoint:
+            - "https://${MY_ECR_REPOSITORY}:5000"
+configs:
+    782340374253.dkr.ecr.us-east-1.amazonaws.com:
+        auth:
+            username: AWS
+            password: ${ECR_TOKEN}
+EOF
+
+sudo mv /tmp/registries.yaml /etc/rancher/k3s/registries.yaml
+
+# reload k3s
+sudo systemctl force-reload k3s
